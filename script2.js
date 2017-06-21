ad25947a5f267dc453456f349b78762551e92c09
Zack P <chips005@gmail.com>
2017-06-20 22:11:55 -0700

    3rd commit

---
+++ script2.js
@@ -0,0 +1,17 @@
+let numbersArray = []  //type each number 0-100?
+
+
+for (var i = 0, i < numbers.length, i++)
+
+if(i === (i/3 == true)) {
+  return Message;
+   print(Message = 'Bing');
+
+} else if (i === (i/5 == true)) {
+  return Message;
+  print(Message = 'Bong');
+  
+} else if (i === (i/3 && i/5 == true)) {
+  return Message;
+  print(Message = 'Bing', 'Bong')
+}
